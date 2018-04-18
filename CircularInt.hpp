5e3b042b90d888abb6e41949ad8ef695aa73feac
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-04-18 13:03:29 +0300

    Create CircularInt.hpp

---
+++ CircularInt.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class CircularInt {
+    
+    int start, end;
+    int curHour;
+    
+    public:
+    
+    CircularInt(int start, int end);
+    CircularInt(int start, int end, int curHour);
+    int getStart();
+    int getEnd();
+    int getCurHour();
+    int setCurHour(int hour);
+    friend ostream& operator<<(ostream& os, const CircularInt& circ);  
+    CircularInt& operator+= (int add);
+    CircularInt& operator++ (int);
+    int operator- ();
+    // friend CircularInt& operator- (int baseHourn, CircularInt& circ);
+    int operator+ (CircularInt& circ);
+    CircularInt& operator*= (int mult);
+    int operator/ (int div);
+};
+
+CircularInt& operator- (int baseHourn, CircularInt& circ);
